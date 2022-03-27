ce94daa2d137d4b5dfa1c692a3e7a3af7144a1a2
Nikola <nikola.wesely.96@gmail.com>
2022-03-27 19:13:09 +0200

    Level 1 + cameras

---
+++ Prison Break/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 3f;
+    [SerializeField] float climbSpeed = 5f;
+
+    Vector2 moveInput;
+    Rigidbody2D rb2d;
+    Animator animator;
+    CapsuleCollider2D capCollider2D;
+    float gravity;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        capCollider2D = GetComponent<CapsuleCollider2D>();
+        gravity = rb2d.gravityScale;
+    }
+
+    void Update()
+    {
+        Run();
+        ClimbLadder();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+        FlipSprite();
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (value.isPressed && capCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            rb2d.velocity += new Vector2(0f, jumpSpeed);
+        }
+    }
+
+    void Run()
+    {
+        rb2d.velocity = new Vector2(moveInput.x*runSpeed, rb2d.velocity.y);
+
+        animator.SetBool("isRunning", Mathf.Abs(moveInput.x) > Mathf.Epsilon);
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorisonatlSpeed = Mathf.Abs(moveInput.x) > Mathf.Epsilon;
+
+        if(playerHasHorisonatlSpeed)
+            transform.localScale = new Vector2(Mathf.Sign(moveInput.x), 1f);
+    }
+
+    void ClimbLadder()
+    {
+        if (capCollider2D.IsTouchingLayers(LayerMask.GetMask("Ladder")))
+        {
+            rb2d.gravityScale = 0f;
+            rb2d.velocity = new Vector2(rb2d.velocity.x, moveInput.y * climbSpeed);
+
+            if (Mathf.Abs(moveInput.y) > Mathf.Epsilon)
+                animator.SetBool("isClimbing", true);
+            else
+                animator.SetBool("isClimbing", false);
+        }
+        else
+        {
+            animator.SetBool("isClimbing", false);
+            rb2d.gravityScale = gravity;
+        }
+    }
+}
